ad665220caf827ebe1f2df07650e8d0a73715661
rumtid <rumtid@users.noreply.github.com>
2017-06-21 16:40:13 +0800

    Move resources to spec/data

--- spec/kdbx_spec.rb
+++ spec/kdbx_spec.rb
@@ -2,49 +2,41 @@ require "spec_helper"
 
 RSpec.describe Kdbx do
   it "has a version number" do
-    expect(Kdbx::VERSION).not_to be_nil
+    expect( Kdbx::VERSION ).to be_a( String )
   end
 
-  context "when loading file" do
-    def resource(name)
-      File.expand_path("../../resource/#{name}", __FILE__)
-    end
-
-    def init_from(filename, **opts)
-      if opts.has_key? :keyfile
-        opts[:keyfile] = resource opts[:keyfile]
-      end
-      Kdbx.new resource(filename), **opts
-    end
-
+  describe "::open" do
     it "accepts null password" do
-      kdbx = init_from "null_pass.kdbx"
-      expect(kdbx.content).to include("secret")
+      kdbx = Kdbx.open data("null_pass.kdbx")
+      expect( kdbx.content ).to include( "secret" )
     end
 
     it "accepts empty password" do
-      kdbx = init_from "empty_pass.kdbx", password: ""
-      expect(kdbx.content).to include("secret")
+      kdbx = Kdbx.open data("empty_pass.kdbx"), password: ""
+      expect( kdbx.content ).to include( "secret" )
     end
 
     it "accepts binary keyfile" do
-      kdbx = init_from "binary_key.kdbx", keyfile: "binary_key.key"
-      expect(kdbx.content).to include("secret")
+      kdbx = Kdbx.open data("binary_key.kdbx"), keyfile: data("binary_key.key")
+      expect( kdbx.content ).to include( "secret" )
     end
 
     it "accepts both password and keyfile" do
-      kdbx = init_from "demo.kdbx", password: "demo", keyfile: "demo.key"
-      expect(kdbx.content).to include("secret")
+      kdbx = Kdbx.open data("demo.kdbx"), password: "demo", keyfile: data("demo.key")
+      expect( kdbx.content ).to include( "secret" )
+    end
+
+    def data(filename)
+      File.expand_path("../data/#{filename}", __FILE__)
     end
   end
 
-  context "when saving database" do
-    it "create empty content" do
-      kdbx, file = Kdbx.new, StringIO.new
-      allow(File).to receive(:open).and_yield(file)
-      expect { kdbx.save }.not_to raise_error
-      file.pos = 0
-      expect(Kdbx.new("").content).to eq("")
+  describe "::new" do
+    it "has empty content" do
+      expect( Kdbx.new.content ).to eq( String.new )
     end
+  end
+
+  describe "#save" do
   end
 end
